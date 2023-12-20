82c0428a548a69504bbdb70263dc7e0fbc5305de
Nicholas Maloney <nick@gnar.dog>
2023-12-20 14:41:26 -0500

    remove unused tests

--- test/test_with_docker.rb
+++ test/test_with_docker.rb
@@ -13,26 +13,6 @@ class WithDockerTest < Minitest::Test
             `docker-compose build --no-cache` if !macos?
           end
 
-  def test_centos_6
-    test_on_x86 with: 'centos_6'
-  end
-
-  def test_centos_7
-    test_on_x86 with: 'centos_7'
-  end
-
-  def test_centos_8
-    test_on_x86 with: 'centos_8'
-  end
-
-  def test_debian_9
-    test_on_x86_and_arm with: 'debian_9'
-  end
-
-  def test_debian_10
-    test_on_x86_and_arm with: 'debian_10'
-  end
-
   def test_debian_11
     test_on_x86_and_arm with: 'debian_11'
   end
@@ -41,28 +21,12 @@ class WithDockerTest < Minitest::Test
     test_on_x86_and_arm with: 'debian_12'
   end
 
-  def test_with_ubuntu_16
-    test_on_x86 with: 'ubuntu_16.04'
-  end
-
-  def test_with_ubuntu_18
-    test_on_x86 with: 'ubuntu_18.04'
-  end
-
   def test_with_ubuntu_20
     test_on_x86 with: 'ubuntu_20.04'
   end
 
   def test_with_ubuntu_22
    test_on_x86 with: 'ubuntu_22.04'
-  end
-
-  def test_with_archlinux
-    test_on_x86 with: 'archlinux'
-  end
-
-  def test_rockylinux_8
-    test_on_x86 with: 'rockylinux_8'
   end
 
   def test_with_macos
